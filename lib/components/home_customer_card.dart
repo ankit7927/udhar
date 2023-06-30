20088b5284d47e2d554e31a8cee0f58352af8a9d
Ankit <code7lancer@gmail.com>
2023-06-30 06:01:46 +0000

    added on tap action on home customer card to navigate to customer page

--- lib/components/home_customer_card.dart
+++ lib/components/home_customer_card.dart
@@ -1,52 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:udhar/pages/customer.dart';
 
 class HomeCustomerCard extends StatelessWidget {
   const HomeCustomerCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 1.6,
-      margin: const EdgeInsets.only(bottom: 12.0),
-      child: Container(
-        padding: const EdgeInsets.fromLTRB(12, 10, 12, 10),
-        child: const Row(
-          children: <Widget>[
-            CircleAvatar(
-              radius: 30.0,
-            ),
-            SizedBox(width: 10.0),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "A title text",
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text("A subtitle text"),
-                ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const Customer()),
+        );
+      }, 
+      child: Card(
+        elevation: 1.6,
+        margin: const EdgeInsets.only(bottom: 12.0),
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(12, 10, 12, 10),
+          child: const Row(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 30.0,
               ),
-            ),
-            SizedBox(width: 10.0),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "100",
-                  style: TextStyle(
-                      fontSize: 32.0, fontWeight: FontWeight.bold, height: 0.8),
+              SizedBox(width: 10.0),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "A title text",
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text("A subtitle text"),
+                  ],
                 ),
-                Text(
-                  "Rs.",
-                  style: TextStyle(fontSize: 8),
-                )
-              ],
-            )
-          ],
+              ),
+              SizedBox(width: 10.0),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "100",
+                    style: TextStyle(
+                        fontSize: 32.0,
+                        fontWeight: FontWeight.bold,
+                        height: 0.8),
+                  ),
+                  Text(
+                    "Rs.",
+                    style: TextStyle(fontSize: 8),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );

6c973e88d73e1acc02078164622cff84cdd30543
ankit <ap8424856@gmail.com>
2023-07-01 10:42:44 +0530

    getting text from text field

--- lib/pages/new_customer.dart
+++ lib/pages/new_customer.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class NewCustomer extends StatefulWidget {
-  const NewCustomer({super.key});
+  NewCustomer({super.key});
+
+  String name = "", address = "";
+  int contact = 0;
+
+  String nameCont = "";
 
   @override
   State<NewCustomer> createState() => _NewCustomerState();
@@ -13,7 +18,15 @@ class _NewCustomerState extends State<NewCustomer> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Add New Customer"),
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.check))],
+        actions: [
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  widget.nameCont = widget.name + widget.contact.toString();
+                });
+              },
+              icon: const Icon(Icons.check))
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.only(left: 18, right: 18, top: 14),
@@ -29,22 +42,36 @@ class _NewCustomerState extends State<NewCustomer> {
                     )),
               ),
               const SizedBox(height: 30),
-              const TextField(
-                decoration: InputDecoration(
+              TextField(
+                onChanged: (String data) {
+                  widget.name = data;
+                },
+                decoration: const InputDecoration(
                     label: Text("Name"), border: OutlineInputBorder()),
               ),
               const SizedBox(height: 10),
-              const TextField(
-                decoration: InputDecoration(
+              TextField(
+                onChanged: (String data) {
+                  widget.contact = int.parse(data);
+                },
+                keyboardType: const TextInputType.numberWithOptions(),
+                decoration: const InputDecoration(
                     label: Text("Contact Number"),
                     border: OutlineInputBorder()),
               ),
               const SizedBox(height: 10),
-              const TextField(
-                keyboardType: TextInputType.numberWithOptions(),
-                decoration: InputDecoration(
+              TextField(
+                onChanged: (String data) {
+                  widget.address = data;
+                },
+                decoration: const InputDecoration(
                     label: Text("Address"), border: OutlineInputBorder()),
               ),
+              const SizedBox(height: 40),
+              Text(
+                widget.nameCont,
+                style: const TextStyle(fontSize: 30),
+              )
             ],
           ),
         ),
